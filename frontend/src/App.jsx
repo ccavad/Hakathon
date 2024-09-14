9705395764adf4f12e7bee68f9a861809501035c
SabinaGarayeva <sgarayeva13@gmail.com>
2024-09-14 23:48:50 +0400

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Dashboard } from "./pages/dashboard/dashboard";
 
 function App() {
-  return <>app</>;
+  return (
+    <ChakraProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </ChakraProvider>
+  );
 }
 
 export default App;
