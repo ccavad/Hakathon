1dc2f3d891583a20787679d1a64baf94e4d0f338
ORxanone <recebovorxan3@gmail.com>
2024-09-15 06:38:50 +0400

    add invate Email board

---
+++ backend/controller/sendMail.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const sendInviteEmail = require("../service/invate.service");
+
+router.post('/invite', async (req, res, next) => {
+    const { email, boardId } = req.body;
+
+    try {
+
+        console.log("req.body: ", req.body)
+        const inviteLink = `${process.env.APP_URL}/invite?boardId=${boardId}`;
+
+        await sendInviteEmail(email, inviteLink);
+
+        res.status(200).json({ message: 'Dəvət emaili göndərildi' });
+
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = router;
